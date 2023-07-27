ed22c21a84eb6409f6903242d966a555d6b7e9e2
kolade0147 <boluwatifekolade94@gmail.com>
2023-07-27 11:32:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+let count = 0
+let save = 0
+
+
+
+function first(){
+count +=1
+countEl.textContent=count
+}
+
+function second(){
+count +=2
+countEl.textContent=count
+}
+
+function third(){
+count +=3
+countEl.textContent=count
+}
+
+function bolu(){
+save +=1
+saveEl.textContent=save
+
+}
+
+function olu(){
+save +=2
+saveEl.textContent=save
+}
+ function lu(){
+ save +=3
+ saveEl.textContent=save
+ 
+ }
+
